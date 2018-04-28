4548fe4eaadd0f9dca9d1989c2f160edfc5d53eb
fmi <fmi@fmi-346-7.fmi.lcl>
2018-04-28 11:35:55 +0300

    asd

--- maven-example/modul1/src/main/java/org/modul1/App.java
+++ maven-example/modul1/src/main/java/org/modul1/App.java
@@ -7,7 +7,12 @@ package org.modul1;
 public class App 
 {
     public static void main( String[] args )
+<<<<<<< Upstream, based on branch 'master' of https://github.com/Dezaro/toDe-et.git
     {
         System.out.println( "Hello Git World!" );
+=======
+    {
+        System.out.println( "Goodbye Git World!" );
+>>>>>>> a908339 asd
     }
 }
